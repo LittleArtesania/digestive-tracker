32341f7d97eed8d33bb8ff482d6c97a7ade413e9
Cristina <20192902@aloe.ulima.edu.pe>
2026-09-04 22:32:29 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/digestive-tracker/sw.js', { scope: '/digestive-tracker/' })})}
